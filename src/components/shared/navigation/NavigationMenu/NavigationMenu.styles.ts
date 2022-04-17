3ed879fa83adb1b41b7361b866ea3ce32f480669
alex.matos <alex.matos@ceuma.br>
2022-04-17 19:47:52 -0300

    refactor: alterando padding do menu de navegação

--- src/components/shared/navigation/NavigationMenu/NavigationMenu.styles.ts
+++ src/components/shared/navigation/NavigationMenu/NavigationMenu.styles.ts
@@ -2,12 +2,10 @@ import styled from "styled-components/native";
 
 export const Container = styled.View`
   width: 100%;
-
-  margin-top: 10px;
 `
 
 export const Wrapper = styled.View`
-  padding: 20px 10px;
+  padding: 15px 10px;
 
   flex-direction: row;
   justify-content: space-between;
